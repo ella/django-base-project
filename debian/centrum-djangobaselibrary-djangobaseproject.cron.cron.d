637acb6428a265e09fc911b097ab29f8e76f1c6f
Ondrej Kohout <ondrej.kohout@centrumholdings.com>
2009-03-12 13:00:08 +0100

    Debianisation added

---
+++ debian/centrum-djangobaselibrary-djangobaseproject.cron.cron.d
@@ -0,0 +1,4 @@
+# cron jobs for centrum-djangobaselibrary-djangobaseproject package
+*/5 * * * * root run-parts /centrum/djangobaselibrary/djangobaseproject/cron/every5min
+10 2 * * * root run-parts /centrum/djangobaselibrary/djangobaseproject/cron/daily
+
